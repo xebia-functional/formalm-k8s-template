289ad50d761081a110a93986838c09cebff4d66b
Victor Carrillo-Redondo <victor.redondo@47deg.com>
2023-07-03 12:54:07 +0200

    missing import

--- .github/workflows/reports-to-comments.js
+++ .github/workflows/reports-to-comments.js
@@ -1,4 +1,5 @@
 module.exports = () => {
+  const fs = require('fs');
   const execSync = require('child_process').execSync;
   const files = execSync('find /home/gradle/reports/ -type f').toString().split('\n').filter(Boolean);
 
@@ -47,33 +48,3 @@ module.exports = () => {
     });
   }
 }
-
-/*
-  } else if (filename.startsWith("suggestion")) {
-    const content = fs.readFileSync(file, 'utf8');
-    const body = content.split("\n");
-    const path = body.shift(); // side-effectful: gets path and removes first line from body
-    const info = filename.split("-");
-    const size = info.length;
-    const startLine = parseInt(info[size - 2]);
-    const endLine = parseInt(info[size - 1]);
-
-    console.log("------DEBUG------");
-    console.log(`path ${path}`);
-    console.log(`start ${startLine}`);
-    console.log(`end ${endLine}`);
-    console.log("-----------------");
-
-    github.rest.pulls.createReviewComment({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      pull_number: context.issue.number,
-      body: body.join("\n"),
-      commit_id: context.payload.pull_request.head.sha,
-      path: path,
-      side: "RIGHT",
-      start_line: startLine,
-      start_side: "RIGHT",
-      line: endLine
-    });
-*/
