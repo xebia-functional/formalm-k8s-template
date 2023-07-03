303d6ea09fea310e3dfe7dacab1c609554bee484
Victor Carrillo-Redondo <victor.redondo@47deg.com>
2023-07-03 12:44:17 +0200

    adding suggestions to pr

--- .github/workflows/reports-to-comments.js
+++ .github/workflows/reports-to-comments.js
@@ -1,22 +1,54 @@
 module.exports = () => {
-  console.log("it works!");
-}
+  const execSync = require('child_process').execSync;
+  const files = execSync('find /home/gradle/reports/ -type f').toString().split('\n').filter(Boolean);
 
-/*const fs = require('fs');
-const execSync = require('child_process').execSync;
-const files = execSync('find /home/gradle/reports/ -type f').toString().split('\n').filter(Boolean);
+  const suggestions = [];
 
-files.forEach(file => {
-  const filenameWithExtension = file.substring(file.lastIndexOf("/") + 1);
-  const filename = filenameWithExtension.substring(0, filenameWithExtension.lastIndexOf("."));
-  if (filename.startsWith("summary")) {
-    const body = fs.readFileSync(file, 'utf8');
-    github.rest.issues.createComment({
-      issue_number: context.issue.number,
+  files.forEach(file => { // report file
+    const filename = file.substring(file.lastIndexOf("/") + 1, file.lastIndexOf("."));
+    if (filename.startsWith("summary")) {
+      const body = fs.readFileSync(file, 'utf8');
+      github.rest.issues.createComment({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: body
+      });
+    } else if (filename.startsWith("suggestion")) {
+      const info = filename.split("-");
+      const startLine = parseInt(info[info.length - 2]);
+      const endLine = parseInt(info[info.length - 1]); // parsing lines from report file's name
+
+      const body = fs.readFileSync(file, 'utf8').split("\n");
+      const targetPath = body.shift(); // side-effectful: gets path and removes first line from body
+
+      const suggestion = {
+        path: targetPath,
+        body: body.join("\n"),
+        start_line: startLine,
+        start_side: "RIGHT"
+        line: endLine,
+        side: "RIGHT"
+      }
+      suggestions.push(suggestion);
+    } else {
+      console.log(`Unexpected file ${filename}`);
+    }
+  });
+
+  if (suggestions.length != 0) {
+    github.rest.pulls.createReview({
       owner: context.repo.owner,
       repo: context.repo.repo,
-      body: body
+      pull_number: context.issue.number,
+      commit_id: context.payload.pull_request.head.sha,
+      body: "Suggestions",
+      comments: suggestions
     });
+  }
+}
+
+/*
   } else if (filename.startsWith("suggestion")) {
     const content = fs.readFileSync(file, 'utf8');
     const body = content.split("\n");
@@ -44,7 +76,4 @@ files.forEach(file => {
       start_side: "RIGHT",
       line: endLine
     });
-  } else {
-    console.log(`Unexpected file ${filename}`);
-  }
-});*/
+*/
